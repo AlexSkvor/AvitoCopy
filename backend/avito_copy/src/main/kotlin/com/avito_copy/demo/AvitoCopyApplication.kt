796db79e72d03469f62d299ee1e5e007ad5cc9d4
AlexSkvor <megoskvor95@yandex.ru>
2019-06-17 21:28:54 +0200

    initial IDEA commit

---
+++ backend/avito_copy/src/main/kotlin/com/avito_copy/demo/AvitoCopyApplication.kt
@@ -0,0 +1,11 @@
+package com.avito_copy.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvitoCopyApplication
+
+fun main(args: Array<String>) {
+    runApplication<AvitoCopyApplication>(*args)
+}
---
+++ backend/avito_copy/src/test/kotlin/com/avito_copy/demo/AvitoCopyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.avito_copy.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvitoCopyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
