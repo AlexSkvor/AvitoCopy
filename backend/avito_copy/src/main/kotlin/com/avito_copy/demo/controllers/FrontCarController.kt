556a1b091c66847c1f1d1ba76e92a095ed40471d
AlexSkvor <megoskvor95@yandex.ru>
2019-06-18 19:55:25 +0200

    getting urls for autos from avito

--- backend/avito_copy/src/main/kotlin/com/avito_copy/demo/controllers/FrontCarController.kt
+++ backend/avito_copy/src/main/kotlin/com/avito_copy/demo/controllers/FrontCarController.kt
@@ -1,10 +1,11 @@
 package com.avito_copy.demo.controllers
 
-import com.avito_copy.demo.entities.front.FrontCar
-import com.avito_copy.demo.entities.front.FrontCarGenerator
 import com.avito_copy.demo.responses.BaseResponse
+import org.jsoup.Jsoup
+import org.jsoup.select.Elements
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -21,24 +22,33 @@ class FrontCarController {
         const val STATUS_ERROR = "error"
         const val CODE_SUCCESS = 200
         const val CODE_AUTH_FAILURE = 401
-
+        const val BAD_REQUEST = 400
 
     }
 
-    @GetMapping
-    fun showStatus(): BaseResponse<String> =
-            BaseResponse(STATUS_SUCCESS, CODE_SUCCESS, arrayOf())
-
-    /*@GetMapping("/filtered")
-    fun filtered(@RequestParam(value = "filterType") filterType: String): BaseResponse<FrontCar> {
-
-    }*/
-
-    @GetMapping("/allCars")
-    fun filtered(): BaseResponse<FrontCar> {
-        val data = FrontCarGenerator.getFrontCarsList(3)
+    @GetMapping("/avito")
+    fun getAvitoCarsList(
+            @RequestParam(value = "pageNumber", required = false, defaultValue = "1") pageNumber: Int,
+            @RequestParam(value = "radius", required = false, defaultValue = "0") radius: Int): BaseResponse<String> {
+        checkPageNumber(pageNumber)?.let { return it }
+        checkRadius(radius)?.let { return it }
+        val data = mutableListOf<String>()
+        val page = Jsoup.connect("https://www.avito.ru/kaliningrad/avtomobili?p=$pageNumber&radius=$radius&f=188_0b0.1375_0b0.1374_0b0.1286_0b0").get()
+        val elements: Elements = page.body().getElementsByClass("js-item-slider item-slider")
+        val links: List<String> = elements.filter { it.hasAttr("href") }.map { it.attr("href") }
+        data.addAll(links)
         return BaseResponse(STATUS_SUCCESS, CODE_SUCCESS, data.toTypedArray())
     }
 
+    private fun checkPageNumber(number: Int): BaseResponse<String>? {
+        return if (number in 1..100) null
+        else BaseResponse(STATUS_ERROR, BAD_REQUEST, arrayOf("Page Number should be between 1 and 100 inclusive, you passed $number"))
+    }
+
+    private fun checkRadius(radius: Int): BaseResponse<String>? {
+        val good = listOf(0, 100, 200, 300, 400, 500, 1000)
+        return if (good.contains(radius)) null
+        else BaseResponse(STATUS_ERROR, BAD_REQUEST, arrayOf("Radius should be one of $good, you passed: $radius"))
+    }
 
 }
