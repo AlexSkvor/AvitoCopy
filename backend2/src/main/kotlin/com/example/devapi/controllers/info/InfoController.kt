e48791cf5c4f1f6f554f986ce951c4a97673cde4
AlexSkvor <megoskvor95@yandex.ru>
2019-06-24 22:34:22 +0200

    mapping over

--- backend2/src/main/kotlin/com/example/devapi/controllers/info/InfoController.kt
+++ backend2/src/main/kotlin/com/example/devapi/controllers/info/InfoController.kt
@@ -5,10 +5,14 @@ import com.example.devapi.utils.STATUS_SUCCESS
 import com.example.devapi.database.dao.CarsDao
 import com.example.devapi.database.dao.LinksDao
 import com.example.devapi.controllers.responses.BaseResponse
+import com.example.devapi.loaders.CityMap
+import com.example.devapi.utils.filesDirectory
+import com.google.gson.Gson
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
 
 /**
  * Created on 6/20/2019
@@ -22,19 +26,35 @@ class InfoController(
         private val linksRepository: LinksDao
 ) {
 
+    private lateinit var mapped: List<String>
+
     @GetMapping("/cars/count")
     fun getTotalMarksNumber(
             @RequestParam(value = "source", required = false, defaultValue = "") source: String
-    ): BaseResponse<Int> =
-            if (source.isEmpty()) BaseResponse(STATUS_SUCCESS, CODE_SUCCESS, arrayOf(carsRepository.count().toInt()))
-            else BaseResponse(STATUS_SUCCESS, CODE_SUCCESS, arrayOf(carsRepository.countBySource(source)))
+    ): BaseResponse<Int> = if (source.isEmpty()) BaseResponse(STATUS_SUCCESS, CODE_SUCCESS, arrayOf(carsRepository.count().toInt()))
+    else BaseResponse(STATUS_SUCCESS, CODE_SUCCESS, arrayOf(carsRepository.countBySource(source)))
 
     @GetMapping("/cars/cities")
     fun getCities(@RequestParam(value = "all", required = false, defaultValue = "false") all: String): BaseResponse<String> {
-        val ans = if (all == "true") linksRepository.getAllCities()
-        else linksRepository.getLoadedCities()
+        val ans = if (all == "true") linksRepository.getAllCities().filter { getMapped().contains(it) }
+        else linksRepository.getLoadedCities().filter { getMapped().contains(it) }
         return BaseResponse(STATUS_SUCCESS, CODE_SUCCESS, ans.toTypedArray())
     }
+
+    private fun getMapped() =
+            if (!::mapped.isInitialized) {
+                mapped = mappedCitiesList()
+                mapped
+            } else mapped
+
+    @GetMapping("/cars/updateCities")
+    fun updateCities(): BaseResponse<String> {
+        mapped = mappedCitiesList()
+        return BaseResponse(STATUS_SUCCESS, CODE_SUCCESS, mapped.toTypedArray())
+    }
+
+    private fun mappedCitiesList() = Gson().fromJson(File(filesDirectory + "cities.txt").readText(), CityMap::class.java)
+            .data.map { it.value }.distinct()
 
     @GetMapping("/cars/tradeMarks")
     fun getTradeMarksList(): BaseResponse<String> =
